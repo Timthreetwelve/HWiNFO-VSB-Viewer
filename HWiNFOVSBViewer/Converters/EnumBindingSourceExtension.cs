8866d1ccfc0c7086c33bc428bae4fc31d84ca37e
Tim Kennedy <timthreetwelve@outlook.com>
2024-12-23 13:47:27 -0600

    Removed getter and unused overload.

--- HWiNFOVSBViewer/Converters/EnumBindingSourceExtension.cs
+++ HWiNFOVSBViewer/Converters/EnumBindingSourceExtension.cs
@@ -14,7 +14,6 @@ internal sealed class EnumBindingSourceExtension : MarkupExtension
     private Type _enumType;
     public Type EnumType
     {
-        get { return _enumType; }
         set
         {
             if (value != _enumType)
@@ -25,13 +24,10 @@ internal sealed class EnumBindingSourceExtension : MarkupExtension
                     if (!enumType.IsEnum)
                         throw new ArgumentException("Type must be for an Enum.");
                 }
-
                 _enumType = value;
             }
         }
     }
-
-    public EnumBindingSourceExtension() { }
 
     public EnumBindingSourceExtension(Type enumType)
     {
