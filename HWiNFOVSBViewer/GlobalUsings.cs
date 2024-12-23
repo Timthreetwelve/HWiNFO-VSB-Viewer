28fe7ad7ae55e955e244f3b80a6ffe696f8962ae
Tim Kennedy <timthreetwelve@outlook.com>
2024-12-22 20:38:02 -0600

    Removed unused using statement.

--- HWiNFOVSBViewer/GlobalUsings.cs
+++ HWiNFOVSBViewer/GlobalUsings.cs
@@ -24,7 +24,6 @@ global using System.Windows.Controls;
 global using System.Windows.Data;
 global using System.Windows.Input;
 global using System.Windows.Media;
-global using System.Windows.Navigation;
 global using System.Windows.Markup;
 
 global using CommunityToolkit.Mvvm.ComponentModel;
