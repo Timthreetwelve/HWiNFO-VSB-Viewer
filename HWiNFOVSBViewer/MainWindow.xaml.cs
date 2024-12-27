031d82d84bb004320c069464581a51c040904a0c
Tim Kennedy <timthreetwelve@outlook.com>
2024-12-26 20:24:32 -0600

    Implemented 'Midnight blue' theme.

--- HWiNFOVSBViewer/MainWindow.xaml.cs
+++ HWiNFOVSBViewer/MainWindow.xaml.cs
@@ -142,6 +142,9 @@ public partial class MainWindow : Window
                         UserSettings.Setting.UITheme = ThemeType.System;
                         break;
                     case ThemeType.System:
+                        UserSettings.Setting.UITheme = ThemeType.DarkBlue;
+                        break;
+                    case ThemeType.DarkBlue:
                         UserSettings.Setting.UITheme = ThemeType.Light;
                         break;
                 }
